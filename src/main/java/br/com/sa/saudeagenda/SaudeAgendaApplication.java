4f86c3b9e4fdb51391dd3b2f4024fa595bee8738
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2023-04-15 12:34:29 -0300

    start project

---
+++ src/main/java/br/com/sa/saudeagenda/SaudeAgendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sa.saudeagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sa/saudeagenda/SaudeAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sa.saudeagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
